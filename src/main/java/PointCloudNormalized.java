3b95eb6ff8cf61774012f3cf52ac9d0a25476889
Satypro <satendra_singh@live.com>
2020-01-26 22:41:57 +0530

    improved and working Algorithms

---
+++ src/main/java/PointCloudNormalized.java
@@ -0,0 +1,83 @@
+import java.io.Serializable;
+
+public class PointCloudNormalized implements Serializable
+{
+    private int  regionid;
+    private long mortoncode;
+    private long pointid;
+    private long x;
+    private long y;
+    private long z;
+
+    public int getRegionid()
+    {
+        return regionid;
+    }
+
+    public void setRegionid(int regionIid)
+    {
+        this.regionid = regionIid;
+    }
+
+    public long getMortoncode()
+    {
+        return mortoncode;
+    }
+
+    public void setMortoncode(long mortoncode)
+    {
+        this.mortoncode = mortoncode;
+    }
+
+    public long getPointid()
+    {
+        return pointid;
+    }
+
+    public void setPointid(long pointid)
+    {
+        this.pointid = pointid;
+    }
+
+    public long getX()
+    {
+        return x;
+    }
+
+    public void setX(long x)
+    {
+        this.x = x;
+    }
+
+    public long getY()
+    {
+        return y;
+    }
+
+    public void setY(long y)
+    {
+        this.y = y;
+    }
+
+    public long getZ()
+    {
+        return z;
+    }
+
+    public void setZ(long z)
+    {
+        this.z = z;
+    }
+
+    public double getDistance()
+    {
+        return distance;
+    }
+
+    public void setDistance(double distance)
+    {
+        this.distance = distance;
+    }
+
+    private double distance;
+}
--- src/main/java/SparkCassandra.java
+++ src/main/java/SparkCassandra.java
@@ -21,7 +21,7 @@ import static com.datastax.spark.connector.japi.CassandraJavaUtil.mapToRow;
 
 public class SparkCassandra
 {
-    public static void Process()
+    public static void BuildDataInCassandra()
     {
         SparkConf conf = new SparkConf()
             .set("spark.cassandra.connection.host", "192.168.29.240")
@@ -40,15 +40,20 @@ public class SparkCassandra
             .getOrCreate();
 
         JavaRDD<String> input = spark.sparkContext()
+
             .textFile("/home/hadoop/sparkinput/3d_4.txt", 2)
             .toJavaRDD();
-
+           //3dsemantic.txt
         JavaRDD<PointCloud> pointClouds = input
             .mapPartitions(new FlatMapFunction<Iterator<String>, PointCloud>()
             {
                 @Override
                 public Iterator<PointCloud> call(Iterator<String> stringIterator) throws Exception
                 {
+                    double p = 0.0001f;
+                    double xMin = 0.984125d;
+                    double yMin = 0.0d;
+                    double zMin = 0.0159803d;
                     List<PointCloud> pointClouds = new ArrayList<>();
                     Morton64 m = new Morton64(3, 21);
                     while (stringIterator.hasNext())
@@ -56,14 +61,18 @@ public class SparkCassandra
                         String in = stringIterator.next();
                         String[] cord = in.split(" ");
                         PointCloud pointCloud = new PointCloud();
-                        float x = Float.parseFloat(cord[1]);
-                        float y = Float.parseFloat(cord[2]);
-                        float z = Float.parseFloat(cord[3]);
+                        double x = Double.parseDouble(cord[1]);
+                        double y = Double.parseDouble(cord[2]);
+                        double z = Double.parseDouble(cord[3]);
+
+                        long xNorm = (long) ((x - xMin)/p);
+                        long yNorm = (long) ((y - yMin)/p);
+                        long zNorm = (long) ((z - zMin)/p);
 
                         long mCode = m.pack(
-                            (int) (x * 1000000),
-                            (int) (y * 1000000),
-                            (int) (z * 1000000)
+                            xNorm,
+                            yNorm,
+                            zNorm
                         );
 
                         pointCloud.setRegionid(1);
@@ -108,73 +117,147 @@ public class SparkCassandra
             ).saveToCassandra();
     }
 
-    public static void PP(JavaRDD<PointCloud> sortedRDD, PointCloud inputPointCloud)
+    public static void BuildDataOutputInCassandra()
     {
-        JavaRDD<PointCloud> candidatePointCloud = sortedRDD
-            .mapPartitions(new FlatMapFunction<Iterator<PointCloud>, PointCloud>()
-            {
-                @Override
-                public Iterator<PointCloud> call(Iterator<PointCloud> pointCloudIterator) throws Exception
-                {
-                    List<PointCloud> pointCloudResult = new ArrayList<PointCloud>();
-                    int k = 10;
-                    int greaterCount = 0;
-                    int lesserCount = 0;
-
-                    while (pointCloudIterator.hasNext())
-                    {
-                        PointCloud pointCloud = pointCloudIterator.next();
-                        if (inputPointCloud.getMortoncode() <= pointCloud.getMortoncode())
+        SparkConf conf = new SparkConf()
+                .set("spark.cassandra.connection.host", "192.168.29.240")
+                .setJars(new String[]
                         {
-                            if (greaterCount < 11)
-                            {
-                                float xdiff = (inputPointCloud.getX() - pointCloud.getX()) * (inputPointCloud
-                                    .getX() - pointCloud.getX());
-                                float ydiff = (inputPointCloud.getY() - pointCloud.getY()) * (inputPointCloud
-                                    .getY() - pointCloud.getY());
-                                float zdiff = (inputPointCloud.getZ() - pointCloud.getZ()) * (inputPointCloud
-                                    .getZ() - pointCloud.getZ());
-                                Long distance = (long) Math.sqrt(xdiff + ydiff + zdiff);
-                                pointCloud.setDistance(distance);
-                                pointCloudResult.add(pointCloud);
-                            }
-                            greaterCount++;
-                        }
+                                "/home/hadoop/IdeaProjects/project/pclspark/build/libs/pclspark-1.0-SNAPSHOT.jar",
+                                "/home/hadoop/.gradle/caches/modules-2/files-2.1/com.datastax.spark/spark-cassandra-connector_2.11/2.4.2/c91029d0882509bedd32877e50e6c2e6528b3e8d/spark-cassandra-connector_2.11-2.4.2.jar",
+                                "/home/hadoop/.gradle/caches/modules-2/files-2.1/com.twitter/jsr166e/1.1.0/233098147123ee5ddcd39ffc57ff648be4b7e5b2/jsr166e-1.1.0.jar"
+                        })
+                .setMaster("spark://192.168.29.106:7077")
+                .setAppName("PCL_PROCESSOR_CASSANDRA_SEARCH");
 
-                        if (inputPointCloud.getMortoncode() >= pointCloud.getMortoncode())
-                        {
-                            if (lesserCount < 11)
-                            {
-                                float xdiff = (inputPointCloud.getX() - pointCloud.getX()) * (inputPointCloud
-                                    .getX() - pointCloud.getX());
-                                float ydiff = (inputPointCloud.getY() - pointCloud.getY()) * (inputPointCloud
-                                    .getY() - pointCloud.getY());
-                                float zdiff = (inputPointCloud.getZ() - pointCloud.getZ()) * (inputPointCloud
-                                    .getZ() - pointCloud.getZ());
-                                Long distance = (long) Math.sqrt(xdiff + ydiff + zdiff);
-                                pointCloud.setDistance(distance);
-                                pointCloudResult.add(pointCloud);
-                            }
-                            lesserCount++;
-                        }
-                    }
-                    return pointCloudResult.iterator();
-                }
-            });
+        SparkSession spark = SparkSession
+                .builder()
+                .config(conf)
+                .getOrCreate();
 
-        JavaRDD<PointCloud> finalKNNPoints = candidatePointCloud
-            .sortBy(
-                new Function<PointCloud, Double>()
+        JavaRDD<String> input = spark.sparkContext()
+
+                .textFile("/home/hadoop/sparkinput/3dsemantic.txt", 2)
+                .toJavaRDD();
+
+        JavaRDD<PointCloudNormalized> pointClouds = input
+                .mapPartitions(new FlatMapFunction<Iterator<String>, PointCloudNormalized>()
                 {
                     @Override
-                    public Double call(PointCloud v1) throws Exception
+                    public Iterator<PointCloudNormalized> call(Iterator<String> stringIterator) throws Exception
                     {
-                        return v1.getDistance();
+                        List<PointCloudNormalized> pointClouds = new ArrayList<>();
+                        Morton64 m = new Morton64(3, 21);
+                        while (stringIterator.hasNext())
+                        {
+                            String in = stringIterator.next();
+                            String[] cord = in.split(" ");
+                            PointCloudNormalized pointCloud = new PointCloudNormalized();
+                            long x = Long.parseLong(cord[1]);
+                            long y = Long.parseLong(cord[2]);
+                            long z = Long.parseLong(cord[3]);
+
+                            long mCode = m.pack(x, y, z);
+
+                            pointCloud.setRegionid(1);
+                            pointCloud.setMortoncode(mCode);
+                            pointCloud.setPointid(Long.parseLong(cord[0]));
+                            pointCloud.setX(x);
+                            pointCloud.setY(y);
+                            pointCloud.setZ(z);
+                            pointClouds.add(pointCloud);
+                        }
+                        return pointClouds.iterator();
                     }
-                },
-                false,
-                1
-            );
+                });
+
+        CassandraJavaUtil.javaFunctions(pointClouds)
+                .writerBuilder(
+                        "PointCloud_Database",
+                        "pointcloudnormalized",
+                        mapToRow(PointCloudNormalized.class)
+                ).saveToCassandra();
+    }
+
+    public static void KNNSearchNormalized(JavaRDD<PointCloudNormalized> sortedRDD, PointCloudNormalized inputPointCloud)
+    {
+        JavaRDD<PointCloudNormalized> candidatePointCloud = sortedRDD
+                .mapPartitions(new FlatMapFunction<Iterator<PointCloudNormalized>, PointCloudNormalized>()
+                {
+                    @Override
+                    public Iterator<PointCloudNormalized> call(Iterator<PointCloudNormalized> pointCloudIterator) throws Exception
+                    {
+                        List<PointCloudNormalized> pointCloudResult = new ArrayList<PointCloudNormalized>();
+
+                        int k = 10;
+                        int firstKCount = 1;
+                        int windowKCount = 1;
+                        int windowStartIndex = 0;
+                        while (pointCloudIterator.hasNext())
+                        {
+                            PointCloudNormalized pointCloud = pointCloudIterator.next();
+                            float xdiff = (inputPointCloud.getX() - pointCloud.getX()) * (inputPointCloud
+                                    .getX() - pointCloud.getX());
+                            float ydiff = (inputPointCloud.getY() - pointCloud.getY()) * (inputPointCloud
+                                    .getY() - pointCloud.getY());
+                            float zdiff = (inputPointCloud.getZ() - pointCloud.getZ()) * (inputPointCloud
+                                    .getZ() - pointCloud.getZ());
+                            double distance = Math.sqrt(xdiff + ydiff + zdiff);
+                            pointCloud.setDistance(distance);
+
+                            // Left Side Partition
+                            if (inputPointCloud.getMortoncode() > pointCloud.getMortoncode())
+                            {
+                                if (firstKCount <= k)
+                                {
+                                    pointCloudResult.add(pointCloud);
+                                    firstKCount++;
+                                }
+
+                                if (firstKCount > k)
+                                {
+                                    if (windowStartIndex == k)
+                                    {
+                                        windowStartIndex = 0;
+                                    }
+                                    pointCloudResult.set(windowStartIndex, pointCloud);
+                                    windowStartIndex++;
+                                }
+                            }
+
+                            //Right Side Partitions
+                            if (inputPointCloud.getMortoncode() <= pointCloud.getMortoncode())
+                            {
+                                if (windowKCount <= k)
+                                {
+                                    pointCloudResult.add(pointCloud);
+                                    windowKCount++;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                        return pointCloudResult.iterator();
+                    }
+                });
+        /*
+        JavaRDD<PointCloudNormalized> finalKNNPoints = candidatePointCloud
+                .sortBy(
+                        new Function<PointCloudNormalized, Double>()
+                        {
+                            @Override
+                            public Double call(PointCloudNormalized v1) throws Exception
+                            {
+                                return v1.getDistance();
+                            }
+                        },
+                        true,
+                        1
+                );
+        */
+
        /*
         CassandraJavaUtil.javaFunctions(finalKNNPoints)
                 .writerBuilder(
@@ -183,17 +266,33 @@ public class SparkCassandra
                         mapToRow(PointCloud.class)
                 ).saveToCassandra();
         */
-        System.out.println(inputPointCloud.getPointid());
+
         /*
+        System.out.println(inputPointCloud.getPointid());
+
         System.out.println(inputPointCloud.getX());
         System.out.println(inputPointCloud.getY());
         System.out.println(inputPointCloud.getZ());
         System.out.println("*********************");
         */
-        List<PointCloud> pointCloudsExisting = finalKNNPoints.collect();
-        for (PointCloud point : pointCloudsExisting)
+
+        List<PointCloudNormalized> pointCloudsExisting = candidatePointCloud.collect();
+        //List<PointCloudNormalized> pointCloudsExisting = finalKNNPoints.collect();
+        for (PointCloudNormalized point : pointCloudsExisting)
         {
             System.out.print(point.getPointid());
+            System.out.print(" ");
+            System.out.print(point.getDistance());
+            System.out.print(" ");
+            System.out.print(point.getX());
+            System.out.print(" ");
+            System.out.print(point.getY());
+            System.out.print(" ");
+            System.out.print(point.getZ());
+            System.out.print(" ");
+            System.out.println(point.getMortoncode());
+
+            /*
             System.out.print(" ");
             System.out.print(point.getX());
             System.out.print(" ");
@@ -201,10 +300,12 @@ public class SparkCassandra
             System.out.print(" ");
             System.out.print(point.getZ());
             System.out.println("");
+            */
         }
+        System.out.println("DONE FIRST");
     }
 
-    public static void PerformKNN(JavaRDD<PointCloud> sortedRDD, PointCloud inputPointCloud)
+    public static void KNNSearch(JavaRDD<PointCloud> sortedRDD, PointCloud inputPointCloud)
     {
         JavaRDD<PointCloud> candidatePointCloud = sortedRDD
             .mapPartitions(new FlatMapFunction<Iterator<PointCloud>, PointCloud>()
@@ -434,45 +535,87 @@ public class SparkCassandra
                 2
             );
 
-        //List<PointCloud> inputPointCloud = sortedRDD.collect();
         sortedRDD.persist(StorageLevel.MEMORY_AND_DISK());
 
-        /*
-        PointCloud pp = inputPointCloud.get(3000);
-        for (int i = 2990; i < 3011; i++)
-        {
-            System.out.println(inputPointCloud.get(i).getPointid());
-            System.out.println(inputPointCloud.get(i).getMortoncode());
-        }
-        */
-
-        /*
-        for (PointCloud pointcloudinput : inputPointCloud)
-        {
-            SparkCassandra.PP(sortedRDD, pointcloudinput);
-        }
-        */
-
-        /*
-        sortedRDD.foreachPartition(new VoidFunction<Iterator<PointCloud>>()
-        {
-            @Override
-            public void call(Iterator<PointCloud> pointCloudIterator) throws Exception
-            {
-                while(pointCloudIterator.hasNext())
-                {
-                    PointCloud p = pointCloudIterator.next();
-                    SparkCassandra.PerformKNN(sortedRDD, p);
-                }
-            }
-        });
-        */
-
         List<PointCloud> inputPointCloud = pointCloudRdd.collect();
-        PointCloud inputPoint = inputPointCloud.get(3000);
+        PointCloud inputPoint = inputPointCloud.get(5000);
         System.out.println(inputPoint.getPointid());
-        SparkCassandra.PerformKNN(sortedRDD, inputPoint);
+        SparkCassandra.KNNSearch(sortedRDD, inputPoint);
 
         System.out.println("DONE PROCESSING");
+    }
+
+    public static void ProcessKNNNormalized()
+    {
+        SparkConf conf = new SparkConf()
+                .set("spark.cassandra.connection.host", "192.168.29.240")
+                .setJars(new String[]
+                        {
+                                "/home/hadoop/IdeaProjects/project/pclspark/build/libs/pclspark-1.0-SNAPSHOT.jar",
+                                "/home/hadoop/.gradle/caches/modules-2/files-2.1/com.datastax.spark/spark-cassandra-connector_2.11/2.4.2/c91029d0882509bedd32877e50e6c2e6528b3e8d/spark-cassandra-connector_2.11-2.4.2.jar",
+                                "/home/hadoop/.gradle/caches/modules-2/files-2.1/com.twitter/jsr166e/1.1.0/233098147123ee5ddcd39ffc57ff648be4b7e5b2/jsr166e-1.1.0.jar"
+                        })
+                .setMaster("spark://192.168.29.106:7077")
+                .setAppName("PCL_PROCESSOR_CASSANDRA_SEARCH");
+
+        SparkSession spark = SparkSession
+                .builder()
+                .config(conf)
+                .getOrCreate();
+
+        SparkContext context = spark.sparkContext();
+
+        CassandraTableScanJavaRDD<PointCloudNormalized> pointCloudTable = CassandraJavaUtil
+                .javaFunctions(context)
+                .cassandraTable(
+                        "PointCloud_Database",
+                        "pointcloudnormalized",
+                        mapRowTo(PointCloudNormalized.class)
+                );
+
+        CassandraTableScanJavaRDD<PointCloudNormalized> pointCloudRdd = pointCloudTable
+                .select("regionid", "mortoncode", "pointid", "x", "y", "z");
+
+        JavaRDD<PointCloudNormalized> sortedRDD = pointCloudRdd
+                .sortBy(
+                        new Function<PointCloudNormalized, Long>()
+                        {
+                            @Override
+                            public Long call(PointCloudNormalized pointCloud) throws Exception
+                            {
+                                return pointCloud.getMortoncode();
+                            }
+                        },
+                        true,
+                        2
+                );
+
+        sortedRDD.persist(StorageLevel.MEMORY_AND_DISK());
+
+        PointCloudNormalized inputPoint = new PointCloudNormalized();
+        inputPoint.setRegionid(1);
+        inputPoint.setPointid(11861481L);
+        inputPoint.setMortoncode(670208810875044L);
+        inputPoint.setX(41008L);
+        inputPoint.setY(108884L);
+        inputPoint.setZ(3931);
+        inputPoint.setDistance(0);
+
+        System.out.println(inputPoint.getPointid());
+        SparkCassandra.KNNSearchNormalized(sortedRDD, inputPoint);
+        System.out.println("DONE PROCESSING1");
+
+        inputPoint = new PointCloudNormalized();
+        inputPoint.setRegionid(1);
+        inputPoint.setPointid(6008243L);
+        inputPoint.setMortoncode(612715888602887L);
+        inputPoint.setX(51849L);
+        inputPoint.setY(90297L);
+        inputPoint.setZ(5309L);
+        inputPoint.setDistance(0);
+
+        System.out.println(inputPoint.getPointid());
+        SparkCassandra.KNNSearchNormalized(sortedRDD, inputPoint);
+        System.out.println("DONE PROCESSING2");
     }
 }
